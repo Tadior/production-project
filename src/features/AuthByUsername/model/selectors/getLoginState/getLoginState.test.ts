f65fc3163180145581631cfd8b0d55a3f3cb54c7
Dmitriy <zamulin1998@gmail.com>
2023-03-27 19:38:38 +0300

    add tests for loginSlice and AuthByUserName selectors

---
+++ src/features/AuthByUsername/model/selectors/getLoginState/getLoginState.test.ts
@@ -0,0 +1,31 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import { getLoginState } from "./getLoginState";
+
+describe("getLoginError", () => {
+  test("should return true", () => {
+    const loginForm = {
+      password: "123",
+      username: "user",
+      isLoading: true,
+      error: "error",
+    };
+    const state: DeepPartial<StateSchema> = {
+      counter: {
+        value: 45,
+      },
+      user: {
+        authData: {
+          id: "red",
+          username: "red",
+        },
+      },
+      loginForm,
+    };
+    expect(getLoginState(state as StateSchema)).toEqual(loginForm);
+  });
+  test("should work with empty state", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginState(state as StateSchema)).toEqual(undefined);
+  });
+});
---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import { getLoginUsername } from "./getLoginUsername";
+
+describe("getLoginError", () => {
+  test("should return true", () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: {
+        username: "name",
+      },
+    };
+    expect(getLoginUsername(state as StateSchema)).toEqual("name");
+  });
+  test("should work with empty state", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginUsername(state as StateSchema)).toEqual("");
+  });
+});
